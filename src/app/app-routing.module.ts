8472f498a4f3fbab70547bc235e445bdbeceb4f8
Evelyn Zepeda <evaynnn@gmail.com>
2025-02-16 21:15:46 -0800

    adding about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LayoutsComponent } from './layouts/layouts.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //The base layout serves as the main page. All others are children being redirected.
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+
+    {
+      path: '',
+      component: HomeComponent,
+      title: "SweetCakes: Home"
+    },
+
+    {
+    path: 'home',
+    component: HomeComponent
+    },
+    {
+      path: 'menu',
+      component: MenuComponent
+    },
+    {
+      path: 'about',
+      component: AboutUsComponent
+    },
+    {
+      path: 'contact',
+      component: ContactComponent
+    }
+      ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutsComponent } from './layouts/layouts.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LayoutsComponent,
+    BaseLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    AboutUsComponent,
+    ContactComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
